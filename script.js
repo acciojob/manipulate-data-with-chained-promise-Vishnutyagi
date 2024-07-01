d8778f725753fb169d16abf039ab61e393143a66
Vishnu tyagi <106895072+Vishnutyagi@users.noreply.github.com>
2024-07-01 17:40:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-//your JS code here. If required.
+function getNumbers() {
+            return new Promise((resolve) => {
+                setTimeout(() => {
+                    resolve([1, 2, 3, 4]);
+                }, 3000);
+            });
+        }
+
+        function filterOddNumbers(numbers) {
+            return new Promise((resolve) => {
+                setTimeout(() => {
+                    const evenNumbers = numbers.filter(number => number % 2 === 0);
+                    document.getElementById('output').innerText = evenNumbers.join(', ');
+                    resolve(evenNumbers);
+                }, 1000);
+            });
+        }
+
+        function multiplyByTwo(numbers) {
+            return new Promise((resolve) => {
+                setTimeout(() => {
+                    const multipliedNumbers = numbers.map(number => number * 2);
+                    document.getElementById('output').innerText = multipliedNumbers.join(', ');
+                    resolve(multipliedNumbers);
+                }, 2000);
+            });
+        }
+
+        getNumbers()
+            .then(numbers => filterOddNumbers(numbers))
+            .then(evenNumbers => multiplyByTwo(evenNumbers))
+            .catch(error => console.error(error));
